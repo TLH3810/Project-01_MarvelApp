89f1a613cfea6773e969ed9e6dcbbacb1f2b6111
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-03-12 23:22:06 -0500

    Added GO back link 2nd index2.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,27 @@
 var charEl = document.getElementById("inputCharacter");
 var submitEl = document.getElementById("submitBtn");
 let movieBtnEl = document.getElementById("movieBtn");
-// var key = cryptedKey(secretKey);
+var movieInputEl = document.getElementById("inputMovie");
+
 secretKey = prompt("Please enter the secret key");
 
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-    console.log(charEl.value);
-  
-    var character = charEl.value;
-    var movieInputEl = $("#inputMovie").val();
-    console.log(movieInputEl);
-    if (character) {
-        getCharacter(character);
-    } else if (movieInputEl){
-      window.location.assign('./index2.html?repo=' + movieInputEl + "?key=" + secretKey);
-    }
-    else{
-      alert('Please enter a character');
-    }
-  };
+function marvelChar(){
+  var character = charEl.value;
+  if (character) {
+    getCharacter(character);
+  } else {
+    alert('Please enter a character');
+  }
+}
 
-function getCharacter(charEl){
-    var requestUrl = 'https://gateway.marvel.com:443/v1/public/characters?name=' + charEl + "&ts=1"  + "&apikey=" + key + "&hash=e2a858abc628bf93fb6ac66501d70db5" ;
-    let charInfo = $(".char-info");
-    fetch(requestUrl)
-    .then(function(response) {
-        return response.json();
-    }).then(function(data){
+function getCharacter(charEl) {
+  var requestUrl = 'https://gateway.marvel.com:443/v1/public/characters?name=' + charEl + "&ts=1" + "&apikey=" + key + "&hash=e2a858abc628bf93fb6ac66501d70db5";
+  let charInfo = $(".char-info");
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
       console.log(data);
       //Getting the character name
       let charName = data.data.results[0].name;
@@ -41,24 +34,30 @@ function getCharacter(charEl){
       let charImageExt = data.data.results[0].thumbnail.extension;
       //Constructing the image path
       let image = charImage + "/portrait_medium." + charImageExt
-       //setting the character name, description and image
-       $("#charName").text(charName);
-       $("#charDesc").text(charDesc);
-       $(".char-image").attr("src",image);
-        let x = data.data.results[0].urls;
-        $.each(x,function (i, item){
-          $("<li/>").html($("<a>").attr("href",item.url).text(charName + " " + item.type)).appendTo(charInfo);
-        });
-      
+      //setting the character name, description and image
+      $("#charName").text(charName);
+      $("#charDesc").text(charDesc);
+      $(".char-image").attr("src", image);
+      let x = data.data.results[0].urls;
+      $.each(x, function (i, item) {
+        $("<li/>").html($("<a>").attr("href", item.url).text(charName + " " + item.type)).appendTo(charInfo);
+      });
+
     })
 }
-submitEl.addEventListener("click", formSubmitHandler);
-
-var key = function cryptedKey(secretKey){
+submitEl.addEventListener("click", marvelChar);
+var key = cryptedKey(secretKey);
+function cryptedKey(secretKey) {
   var bytes = CryptoJS.AES.decrypt("U2FsdGVkX1+e5165l/K6go5vrvAMZlD+BURW1Nj4RUlmkl8sNkj4IiODCzBkJlniNHUdQNjfyVfBCPeyh/isAQ==", secretKey.toString());
   plaintext = bytes.toString(CryptoJS.enc.Utf8);
+  console.log(plaintext);
   return plaintext;
 }
 
+movieBtnEl.addEventListener("click",  movieSubmitForm);
 
-movieBtnEl.addEventListener("click", formSubmitHandler);
+function movieSubmitForm(){
+  var movieInputEl = $("#inputMovie").val();
+  window.location.assign('./index2.html?repo=' + movieInputEl + "?key=" + secretKey)
+
+}
